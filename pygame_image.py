98a1fe9b1ef9993b74d70184ec8d689adbbafbdc
c0a2405612 <c0a2405612@edu.teu.ac.jp>
2025-06-10 17:04:43 +0900

    練習4

--- pygame_image.py
+++ pygame_image.py
@@ -11,13 +11,14 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.image.load("fig/3.png")
-    kk_img = pg.transform.flip(kk_img, True, True)
+    kk_img = pg.transform.flip(kk_img, True, False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        #練習2 
+            
         screen.blit(bg_img, [0, 0])
+        screen.blit(kk_img, [300,200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
