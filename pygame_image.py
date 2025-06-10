b7b863872cf3de26d29367aab88dc2cedb936938
c0a2405612 <c0a2405612@edu.teu.ac.jp>
2025-06-10 17:07:42 +0900

    練習7

--- pygame_image.py
+++ pygame_image.py
@@ -17,7 +17,9 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
             
-        screen.blit(bg_img, [-tmr, 0])
+        x = tmr    
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img, [-x+1600, 0])
         screen.blit(kk_img, [300,200])
         pg.display.update()
         tmr += 1        
