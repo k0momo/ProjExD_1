3c39f1b1f5cdf2ffb00211d626479b40bb84d6b4
c0a2405612 <c0a2405612@edu.teu.ac.jp>
2025-06-10 17:09:30 +0900

    練習8

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     tmr = 0
@@ -19,7 +20,7 @@ def main():
             
         x = tmr    
         screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img, [-x+1600, 0])
+        screen.blit(bg_img2, [-x+1600, 0])
         screen.blit(kk_img, [300,200])
         pg.display.update()
         tmr += 1        
