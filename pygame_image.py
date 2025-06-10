d3e0ff4ae2437911b7996d99534642a88267e740
c0a2405612 <c0a2405612@edu.teu.ac.jp>
2025-06-10 17:36:54 +0900

    演習1

--- pygame_image.py
+++ pygame_image.py
@@ -19,21 +19,13 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        key_lst = pg.key.get_pressed()  
-        if key_lst[pg.K_UP]:  
-            kk_rct.move_ip(0, -1)  
-        if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip(0, +1)
-        if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip(-1, 0)
-        if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip(+1, 0)
-            
-        x = tmr%3200
-        screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img2, [-x+1600, 0])
-        screen.blit(bg_img, [-x+3200, 0])
-        # screen.blit(kk_img, [300,200])
+        key_lst = pg.key.get_pressed()
+        speed = 2 if not key_lst[pg.K_RIGHT] else -2
+        w = bg_img.get_width()
+        x = (tmr * speed) % (w * 2)
+        screen.blit(bg_img,  [-x,      0])
+        screen.blit(bg_img2, [-x +  w, 0])
+        screen.blit(bg_img,  [-x + 2*w,0])
         screen.blit(kk_img, kk_rct)
         pg.display.update()
         # print(tmr, x)
