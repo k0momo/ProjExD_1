bd24ba0d109530dda26afccffcbf23bb07182969
c0a2405612 <c0a2405612@edu.teu.ac.jp>
2025-06-10 17:53:52 +0900

    演習2

--- pygame_image.py
+++ pygame_image.py
@@ -9,43 +9,38 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
-    
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    bg_img2 = pg.transform.flip(bg_img, True, False)
-    
+    bg_img_flip = pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
-    kk_rct = kk_img.get_rect()  
+    kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
-    
+
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-            
-            
-        key_lst = pg.key.get_pressed()
-        
-        speed = 2 if not key_lst[pg.K_RIGHT] else -2
-        
-        dx = dy = 0
-        if   key_lst[pg.K_UP]:    dy = -1
-        elif key_lst[pg.K_DOWN]:  dy = +1
-        if   key_lst[pg.K_LEFT]:  dx = -1
-        elif key_lst[pg.K_RIGHT]: dx = +1
-        kk_rct.move_ip(dx, dy)
-        
-        w = bg_img.get_width()
-        x = (tmr * speed) % (w * 2)
-    
-        screen.blit(bg_img,  [-x,      0])
-        screen.blit(bg_img2, [-x +  w, 0])
-        screen.blit(bg_img,  [-x + 2*w,0])
-        
+        x = tmr%3200
+        screen.blit(bg_img, [x, 0])
+        screen.blit(bg_img_flip, [-1600+x, 0])
+        screen.blit(bg_img, [-3200+x, 0])
+        # screen.blit(kk_img, [300, 200])
         screen.blit(kk_img, kk_rct)
+
+        key_lst = pg.key.get_pressed()
+        dx, dy = 0, 0
+        if key_lst[pg.K_UP]:
+            dy += -1
+        elif key_lst[pg.K_DOWN]:
+            dy += 1
+        elif key_lst[pg.K_LEFT]:
+            dx += -1
+        elif key_lst[pg.K_RIGHT]:
+            dx += 2
+        kk_rct.move_ip(dx-1, dy)
+
         pg.display.update()
-        # print(tmr, x)
         tmr += 1        
         clock.tick(200)
 
